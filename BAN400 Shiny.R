2024357d5bc94578c5b41c5f0283abf93e769bbe
Bendik Brunvoll <bendik.brunvoll@student.nhh.no>
2020-11-23 12:36:21 +0100

    Updated structure for Shiny App

--- BAN400 Shiny.R
+++ BAN400 Shiny.R
@@ -1,2 +1,38 @@
 rm(list = ls())
-# hei
+
+#### Libraries
+require(tidyverse)
+require(shiny)
+
+# Building Shiny App
+
+ui <- fluidPage(
+  textInput(inputId = "text",
+            label = "Search stock",
+            value = "",
+            width = NULL,
+            placeholder = NULL),
+  sliderInput(inputId = "RSI", 
+              label = "Relative Strength Index (RSI)",
+              value = 14,
+              min = 1, 
+              max = 30),
+  sliderInput(inputId = "MA", 
+              label = "Moving Average (MA)",
+              value = 50,
+              min = 1, 
+              max = 200),
+  actionButton(inputId = "calc", label = "Click here to meet milfs in your neighbourhood"),
+  plotOutput("hist")
+)
+
+server <- function(input, output){
+  output$hist <- renderPlot({
+    hist(rnorm(input$MA),
+         main = "Test")
+    })
+}
+
+shinyApp(ui = ui, server = server)
+
+
