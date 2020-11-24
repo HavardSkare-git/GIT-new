778679eb859da99d9694c1ce0e793753548ac027
Bendik Brunvoll <bendik.brunvoll@student.nhh.no>
2020-11-24 23:14:44 +0100

    Added Moving Average and page layout

--- BAN400 Shiny.R
+++ BAN400 Shiny.R
@@ -5,14 +5,16 @@ library(tidyquant)
 
 require(tidyverse)
 require(shiny)
+require(shinythemes)
 require(rvest)
 require(XML)
 require(httr)
 require(magrittr)
-require(ggplot2)
 require(xtable)
-library(mar)
-
+require(ggplot2)
+require(ggthemes)
+install.packages("shinythemes")
+install.packages("ggthemes")
 
 
 OBX <- 
@@ -26,69 +28,87 @@ OBX <-
   map_df(~paste0(.,".OL")) 
 
 
-price <- function(ticker, p){
-  outdata <- getSymbols(ticker, 
+price <- function(ticker, n_rsi, n_ma){
+  outdata <- suppressWarnings(getSymbols(ticker, 
                         from = Sys.Date() %m+% months(-12), 
                         to = Sys.Date(), 
-                        warnings = FALSE,
-                        auto.assign = FALSE) %>% 
-    na.omit()
+                        warnings = NULL,
+                        auto.assign = FALSE))
   
   outdata <- data.frame(dates = index(outdata), coredata(outdata)) %>% 
-    select("dates", contains("Close")) 
+    select("dates", contains("Close")) %>% 
+    na.omit()
 
-  outdata$rsi <- RSI(outdata[,2], n = p)
+  outdata$rsi <- RSI(outdata[,2], 
+                     n = n_rsi)
   
-
+  outdata$ma <- rollmean(outdata[,2], 
+                         k = n_ma, 
+                         fill = list(NA, NULL, NA),
+                         align = "right")
+  
   return(outdata)
 }
 
-
-
-
-
+test <- price("AKER.OL", 7, 50)
 # ------------Building Shiny App 
 
-ui <- fluidPage(
-  selectInput(inputId = "stockname",
-              label = "Search stocks",
-              choices = OBX$Ticker,
-              selected = NULL,
-              multiple = FALSE,
-              selectize = TRUE),
-  sliderInput(inputId = "RSI", 
-              label = "Relative Strength Index (RSI)",
-              value = 14,
-              min = 1, 
-              max = 30),
-  sliderInput(inputId = "MA", 
-              label = "Moving Average (MA)",
-              value = 50,
-              min = 1, 
-              max = 200),
-  plotOutput("hist"),
-  plotOutput("rsiplot")
-)
+ui <- navbarPage("BAN400 Project",
+                 tabPanel("COMPANY SEARCH",
+                          fluidPage(theme = shinytheme("superhero"),
+                                    titlePanel("COMPANY SEARCH"),
+                                    sidebarLayout(
+                                      sidebarPanel(
+                                        selectInput(inputId = "stockname",
+                                                    label = "Search stocks",
+                                                    choices = OBX$Ticker,
+                                                    selected = NULL,
+                                                    multiple = FALSE,
+                                                    selectize = TRUE),
+                                        br(),
+                                        sliderInput(inputId = "RSI", 
+                                                    label = "Relative Strength Index (RSI)",
+                                                    value = 14,
+                                                    min = 1, 
+                                                    max = 30),
+                                        sliderInput(inputId = "MA", 
+                                                    label = "Moving Average (MA)",
+                                                    value = 50,
+                                                    min = 1, 
+                                                    max = 200)
+                                      ),
+                                      mainPanel(
+                                        plotOutput("priceplot"),
+                                        br(),
+                                        plotOutput("rsiplot")
+                                      )
+                                    )
+                            
+                          )),
+                 tabPanel("TRADING OPPORTUNITIES"),
+                 tabPanel("ABOUT"))
 
 server <- function(input, output){
   
   data <- reactive({
-    price(input$stockname,input$RSI)
+    suppressWarnings(price(input$stockname,input$RSI, input$MA))
   })
   
-  output$hist <- renderPlot({
+  output$priceplot <- renderPlot({
     data = data()
     ggplot(data)+
       geom_line(aes(
         x = data[,1],
         y = data[,2]))+
+      geom_line(aes(
+        x = data[,1],
+        y = data[,4]),
+        color = "green")+
       xlab("DATE")+
       ylab("CLOSING PRICE")+
-      ggtitle(input$stockname)+
-      theme_bw()
+      ggtitle(paste0("PRICE CHART: ",input$stockname))+
+      theme_economist()
   })
-  
-  
   
   output$rsiplot <- renderPlot({
     data = data()
@@ -96,13 +116,16 @@ server <- function(input, output){
       geom_line(aes(
         x = data[,1],
         y = data[,3]))+
+      geom_hline(yintercept = c(30,70), 
+                 col = "red",
+                 linetype = "dotted")+
       xlab("DATE")+
       ylab("RSI")+
-      ggtitle(input$stockname)+
-      theme_bw()
+      ggtitle(paste0("RSI CHART: ", input$stockname))+
+      ylim(c(15,85))+
+      theme_economist()
   })
 }
 
 shinyApp(ui = ui, server = server)
-
 
