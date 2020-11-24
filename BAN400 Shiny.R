4dfbd214d4eb06dc8ffeeea4e0ccb621d9b092ea
Bendik Brunvoll <bendik.brunvoll@student.nhh.no>
2020-11-24 14:59:22 +0100

    Making the server respond to ticker input from UI

--- BAN400 Shiny.R
+++ BAN400 Shiny.R
@@ -3,15 +3,34 @@ rm(list = ls())
 #### Libraries
 require(tidyverse)
 require(shiny)
+require(rvest)
+require(XML)
+require(httr)
+require(margrittr)
+require(ggplot2)
+require(xtable)
 
-# Building Shiny App
+OBX <- 
+  read_html("https://no.wikipedia.org/wiki/OBX-indeksen") %>% 
+  html_nodes(xpath = '//*[@id="mw-content-text"]/div[1]/table[1]') %>% 
+  html_table() %>% 
+  as.data.frame() %>% 
+  select("Tickersymbol") %>% 
+  set_colnames(c("Ticker")) %>% 
+  map_df(~gsub("OSE: ", "",.)) %>% 
+  map_df(~paste0(.,".OL")) 
+
+
+
+# ------------Building Shiny App 
 
 ui <- fluidPage(
-  textInput(inputId = "text",
-            label = "Search stock",
-            value = "",
-            width = NULL,
-            placeholder = NULL),
+  selectInput(inputId = "stockname",
+              label = "Search stocks",
+              choices = c("",OBX$Ticker),
+              selected = NULL,
+              multiple = FALSE,
+              selectize = TRUE),
   sliderInput(inputId = "RSI", 
               label = "Relative Strength Index (RSI)",
               value = 14,
@@ -22,19 +41,13 @@ ui <- fluidPage(
               value = 50,
               min = 1, 
               max = 200),
-  actionButton(inputId = "calc", label = "Click here"),
   plotOutput("hist")
 )
 
 server <- function(input, output){
-  output$hist <- renderPlot({
-    hist(rnorm(input$MA),
-         main = "Test")
-    })
+
+  output$hist <- renderText(print(input$stockname))
 }
 
 shinyApp(ui = ui, server = server)
-
-#yolo 
-
 
